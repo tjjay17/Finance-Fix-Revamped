4d8c4bf5c5b7f17353124677f74a3fdbbfa46356
tjjay17 <56407501+tjjay17@users.noreply.github.com>
2021-06-17 18:42:59 -0400

    fixed intersection observer effect

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -9,13 +9,13 @@ const Home = () =>{
         document.title = 'Finance-Fix';
     });
 
-    const {ref, inView, entry} = useInView({threshold:0.75, root:null});
+    const {ref, inView, entry} = useInView({threshold:0.6, root:null});
 
-    if(inView){
-        let elem = document.getElementById('features');
-        elem.style.opacity = 1;
-        elem.style.transform = 'translateY(30px)';
-    }
+    // if(inView){
+    //     let elem = document.getElementById('features');
+    //     elem.style.opacity = 1;
+    //     elem.style.transform = 'translateY(30px)';
+    // }
    
     let history = useHistory();
     
@@ -31,7 +31,7 @@ const Home = () =>{
         <div id = 'homeContainer'>
             <h1 id = 'h'>Finance - Fix</h1>
             <img id = 'chartImg' src = '/assets/financedata.svg' alt = 'chart' />
-            <div id = 'features' ref = {ref}>
+            <div id = 'features' ref = {ref} style = {inView ? {opacity:'1', transform:'translateY(30px)'} : {opacity:'0'}}>
                     <FeatureCard src = '/assets/blueDollar.svg' feature = {'Keep Track Of How Much You Spend And Adjust.'}/>
                     <FeatureCard src = '/assets/plaid.svg' feature = {'Optionally Use Plaid To Directly Integrate Your Bank Account.'}/>
                     <FeatureCard src = '/assets/secure.svg' feature = {'Securely Use A Platform With Friendly UI.'} />
